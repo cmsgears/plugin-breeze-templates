61185df4e406de24b3d43c718c0be7098f41ec7a
Lokendra Chouhan <lokendra.chouhan@vulpinecode.com>
2018-10-04 18:03:43 +0530

    updated code

--- templates/page/default/includes/options.php
+++ templates/page/default/includes/options.php
@@ -12,7 +12,7 @@ $htmlOptions	= isset( $template ) && !empty( $template->htmlOptions ) ? json_dec
 $options = [ "data-slug" => $model->slug, "cmt-block" => "block-half-auto" ];
 $options = !empty( $htmlOptions ) ? ArrayHelper::merge( $options, $htmlOptions ) : $options;
 
-$optionClass = $options[ 'class' ];
+$optionClass = $options[ 'class' ] ?? null;
 
 $options[ 'class' ] = isset( $options[ 'class' ] ) ? "page {$optionClass} $tclass page-model-{$template->type} page-{$model->slug}" : "page page-basic page-default $tclass page-model-{$template->type} page-{$model->slug}";
 
