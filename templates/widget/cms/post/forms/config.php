48ee8377b473fcc8fd1156d0d260459b7892fc40
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-12-13 21:04:20 +0530

    Applied quick fixes.

--- templates/widget/cms/post/forms/config.php
+++ templates/widget/cms/post/forms/config.php
@@ -13,12 +13,14 @@ $widgetOptions = [
 	'recent' => 'recent', 'related' => 'related',
 	'similar' => 'similar', 'category' => 'category', 'tag' => 'tag'
 ];
+
+$defaultConfig = Yii::getAlias( '@breeze' ) . '/templates/widget/config';
 ?>
 <div class="box-crud-wrap row">
 	<div class="box-crud-wrap-main row">
 		<?php $form = ActiveForm::begin( [ 'id' => 'frm-config', 'options' => [ 'class' => 'form' ] ] ); ?>
-		<?php include dirname( dirname( __DIR__ ) ) . '/config/basic.php'; ?>
-		<?php include dirname( dirname( __DIR__ ) ) . '/config/paging.php'; ?>
+		<?php include "$defaultConfig/basic.php"; ?>
+		<?php include "$defaultConfig/paging.php"; ?>
 		<div class="box box-crud">
 			<div class="box-header">
 				<div class="box-header-title">Page Config</div>
