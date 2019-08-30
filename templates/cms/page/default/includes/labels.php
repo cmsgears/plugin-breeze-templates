7f2038294624d64d6c4ca82321a7033e7994af88
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2019-08-30 18:59:58 +0530

    Applied quick fixes.

--- templates/cms/page/default/includes/labels.php
+++ templates/cms/page/default/includes/labels.php
@@ -7,9 +7,22 @@ use yii\helpers\Url;
 		<div class="h4">Labels</div>
 		<hr/>
 		<div class="filler-height"></div>
-		<ul class="nav">
-			<?= $model->getCategoryLinks( Url::toRoute( [ '/blog/category' ], true ) ) ?>
-			<?= $model->getTagLinks( Url::toRoute( [ '/blog/tag' ], true ) ) ?>
+		<ul class="nav nav-label">
+			<?php
+				$labels = '';
+
+				if( count( $model->activeCategories ) > 0 ) {
+
+					$labels .= $model->getCategoryLinks( Url::toRoute( [ '/blog/category' ], true ) );
+			?>
+			<?php } ?>
+			<?php
+				if( count( $model->activeTags ) > 0 ) {
+
+					$labels .= $model->getTagLinks( Url::toRoute( [ '/blog/tag' ], true ) );
+			?>
+			<?php } ?>
+			<?= strip_tags( $labels, '<li><a>' ) ?>
 		</ul>
 	</div>
 <?php } ?>
