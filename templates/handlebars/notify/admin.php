4a12250e9689feda9de22dbe3a8f726134051601
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-08-25 09:34:27 +0530

    Applied quick fixes.

--- templates/handlebars/notify/admin.php
+++ templates/handlebars/notify/admin.php
@@ -16,11 +16,11 @@
 
 	{{#if data.adminLink}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.adminLink}}">
-			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><b><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
+			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}">
-			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><b><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
+			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
 		</li>
 	{{/if}}
 
@@ -30,11 +30,11 @@
 
 	{{#if data.adminLink}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.adminLink}}">
-			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><b><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
+			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}">
-			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><b><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
+			<span class="cmt-click reader {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><em class="h6">{{{data.title}}}</em><br>{{{data.content}}}</span><hr>
 		</li>
 	{{/if}}
 
