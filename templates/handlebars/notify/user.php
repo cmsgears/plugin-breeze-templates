fd590258fed5a3af37c15152521c1ec93d3055b0
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2021-01-23 14:23:11 +0530

    Applied quick fixes.

--- templates/handlebars/notify/user.php
+++ templates/handlebars/notify/user.php
@@ -2,11 +2,11 @@
 
 	{{#if data.link}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/notification/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="notification"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="notification"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/notification/read?id={{data.id}}" consumed="{{data.consumed}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="notification"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="notification"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
 
@@ -16,11 +16,11 @@
 
 	{{#if data.link}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
 
@@ -30,11 +30,11 @@
 
 	{{#if data.link}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="activity"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="activity"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="activity"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="activity"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
 
@@ -44,11 +44,11 @@
 
 	{{#if data.link}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/announcement/read?id={{data.id}}" redirect="{{siteUrl}}{{data.link}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 		{{else}}
 		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="notify/announcement/read?id={{data.id}}">
-			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{data.title}}</b> - {{data.content}}</span>
+			<span class="cmt-click {{#if data.consumed  }} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
 
