bedee7149a6420af599c93e3406d51eb255fbf61
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2021-07-31 12:25:32 +0530

    Applied quick fixes.

--- templates/handlebars/notify/org.php
+++ templates/handlebars/notify/org.php
@@ -1,11 +1,11 @@
 <script id="orgNotificationData" type="text/x-handlebars-template">
 
 	{{#if data.link}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/notification/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/notification/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="notification"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{else}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/notification/read?id={{data.id}}" consumed="{{data.consumed}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/notification/read?id={{data.id}}" consumed="{{data.consumed}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="notification"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
@@ -15,11 +15,11 @@
 <script id="orgReminderData" type="text/x-handlebars-template">
 
 	{{#if data.link}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{else}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/reminder/read?id={{data.id}}" consumed="{{data.consumed}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="reminder"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
@@ -29,11 +29,11 @@
 <script id="orgActivityData" type="text/x-handlebars-template">
 
 	{{#if data.link}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/activity/read?id={{data.id}}" consumed="{{data.consumed}}" redirect="{{siteUrl}}{{data.link}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{else}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/activity/read?id={{data.id}}" consumed="{{data.consumed}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/activity/read?id={{data.id}}" consumed="{{data.consumed}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="activity"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
@@ -43,11 +43,11 @@
 <script id="orgAnnouncementData" type="text/x-handlebars-template">
 
 	{{#if data.link}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/announcement/read?id={{data.id}}" redirect="{{siteUrl}}{{data.link}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/announcement/read?id={{data.id}}" redirect="{{siteUrl}}{{data.link}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{else}}
-		<li cmt-app="notify" cmt-controller="notification" cmt-action="hread" action="org/notify/announcement/read?id={{data.id}}">
+		<li cmt-app="orgNotify" cmt-controller="notification" cmt-action="hread" action="orgnotify/announcement/read?id={{data.id}}">
 			<span class="cmt-click {{#if data.consumed}} text text-gray {{else}} link {{/if}}" type="announcement"><b>{{{data.title}}}</b> - {{{data.content}}}</span>
 		</li>
 	{{/if}}
